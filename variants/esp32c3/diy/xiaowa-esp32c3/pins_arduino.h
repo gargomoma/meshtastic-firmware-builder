fd7996defa627e4e54f46604ee1f5d334d7bcfe7
gargomoma <gargomoma@users.noreply.github.com>
2026-06-10 23:19:28 +0200

    add xiaowa-esp32c3

---
+++ variants/esp32c3/diy/xiaowa-esp32c3/pins_arduino.h
@@ -0,0 +1,26 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+static const uint8_t TX = 6;
+static const uint8_t RX = 5;
+
+static const uint8_t SDA = 7;
+static const uint8_t SCL = 8;
+
+static const uint8_t SS = 1;
+static const uint8_t MOSI = 2;
+static const uint8_t MISO = 4;
+static const uint8_t SCK = 3;
+
+/*
+static const uint8_t A0 = 0;
+static const uint8_t A1 = 1;
+static const uint8_t A2 = 2;
+static const uint8_t A3 = 3;
+static const uint8_t A4 = 4;
+static const uint8_t A5 = 5;
+*/
+
+#endif /* Pins_Arduino_h */
